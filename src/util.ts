e718c20cf196ee68ee50d70315dbc1703882eb2e
henry-512 <henrymt512@gmail.com>
2024-08-26 11:33:13 -0400

    tic update

--- src/util.ts
+++ src/util.ts
@@ -10,11 +10,12 @@ export function concat(...str: Array<string | null | undefined | boolean>) {
 export function webgl_support() {
   try {
     var canvas = document.createElement('canvas')
-    return (
-      !!window.WebGLRenderingContext &&
+    return !!(
+      window.WebGLRenderingContext &&
       (canvas.getContext('webgl') || canvas.getContext('experimental-webgl'))
     )
   } catch (e) {
+    console.log(e)
     return false
   }
 }
