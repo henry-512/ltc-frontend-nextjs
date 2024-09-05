cb13bbf62db32a6df6ec396cf06a80b858d85a00
henry-512 <henrymt512@gmail.com>
2024-09-05 13:38:59 -0400

    change config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
-import type { NextConfig } from 'next'
-
-const nextConfig: NextConfig = {
+// @ts-check
+ 
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
   async redirects() {
     return [
       {
